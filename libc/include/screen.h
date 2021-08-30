fe35d5654fb1f1f1c698e9c7978ac84cf69b0802
LahmerMohammed <tonysycho14@gmail.com>
2021-08-30 18:09:22 +0100

    fix

--- libc/include/screen.h
+++ libc/include/screen.h
@@ -28,9 +28,9 @@ void scrollUp(uint8 no_lines);
 
 void lastLineReach();
 
-void write_char(char ch , uint8 color);
+void write_char(char ch);
 
-void write(string str);
+void write(string str , uint8 _color);
 
 uint8 getTextColor(uint8 bg , uint8 fg);
 
